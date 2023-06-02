a29aff1c3dfb34c193e91492bc06b76276e78022
Bojan Maksimovic <bojanm245@gmail.com>
2023-06-03 00:03:37 +0200

    Add a response generator for the entity not found exception

---
+++ FitLog.Api/ExceptionHandling/ResponseGenerators/EntityNotFoundExceptionResponseGenerator.cs
@@ -0,0 +1,17 @@
+﻿using FitLog.Api.ExceptionHandling.Abstraction;
+using FitLog.Application.Exceptions;
+
+namespace FitLog.Api.ExceptionHandling.ResponseGenerators
+{
+    public class EntityNotFoundExceptionResponseGenerator : ExceptionResponseGenerator<EntityNotFoundException>
+    {
+        protected override ExceptionResponse GenerateAfterCast(EntityNotFoundException ex)
+        {
+            return new ExceptionResponse
+            {
+                StatusCode = StatusCodes.Status404NotFound,
+                ShouldBeLogged = false
+            };
+        }
+    }
+}
