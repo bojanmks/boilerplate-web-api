f826b569710b34fb640999d9bb5f4e926217f0ca
Bojan Maksimovic <bojanm245@gmail.com>
2024-02-27 20:55:15 +0100

    Add missing translations

---
+++ WebApi.DataAccess/DbScripts/20240227_AddTranslations.sql
@@ -0,0 +1,14 @@
+insert into Translations([Key], Value, Locale)
+values
+('isRequired', 'Field is required', 'en-US'),
+('isRequired', 'Polje je obavezno', 'sr-Latn-RS'),
+('minimumLength', 'Must be longer than {0} characters', 'en-US'),
+('minimumLength', 'Mora biti duže od {0} karaktera', 'sr-Latn-RS'),
+('invalidEmailAddressFormat', 'Invalid email address format', 'en-US'),
+('invalidEmailAddressFormat', 'Pogrešan format email adrese', 'sr-Latn-RS'),
+('invalidCredentials', 'Invalid credentials', 'en-US'),
+('invalidCredentials', 'Pogrešni kredencijali', 'sr-Latn-RS'),
+('invalidSortStringFormat', 'Invalid sort string format (PropertyName1.Direction,PropertyName2.Direction,...)', 'en-US'),
+('invalidSortStringFormat', 'Pogrešan format teksta za sortiranje (Svojstvo1.Smer,Svojstvo2.Smer...)', 'sr-Latn-RS'),
+('invalidSortDirection', 'Invalid sort direction (Asc,Desc)', 'en-US'),
+('invalidSortDirection', 'Pogrešan smer sortiranja (Asc,Desc)', 'sr-Latn-RS')
