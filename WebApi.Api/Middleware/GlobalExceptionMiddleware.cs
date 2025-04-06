635047972cb31f688703e46fa12387965bad6f45
Bojan Maksimovic <bojanm245@gmail.com>
2025-04-06 10:37:59 +0200

    Update the global exception middleware to also return an EndpointResult in body

--- WebApi.Api/Middleware/GlobalExceptionMiddleware.cs
+++ WebApi.Api/Middleware/GlobalExceptionMiddleware.cs
@@ -1,4 +1,9 @@
-﻿using WebApi.Application.Logging;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using WebApi.Api.Endpoints;
+using WebApi.Application.Localization;
+using WebApi.Application.Logging;
+using WebApi.Application.UseCases;
 
 namespace WebApi.Api.Middleware
 {
@@ -6,11 +11,16 @@ namespace WebApi.Api.Middleware
     {
         private readonly RequestDelegate _next;
         private readonly IExceptionLogger _exceptionLogger;
+        private readonly ITranslator _translator;
 
-        public GlobalExceptionMiddleware(RequestDelegate next, IExceptionLogger exceptionLogger)
+        public GlobalExceptionMiddleware(
+            RequestDelegate next,
+            IExceptionLogger exceptionLogger,
+            ITranslator translator)
         {
             _next = next;
             _exceptionLogger = exceptionLogger;
+            _translator = translator;
         }
 
         public async Task Invoke(HttpContext httpContext)
@@ -30,7 +40,27 @@ namespace WebApi.Api.Middleware
             await _exceptionLogger.Log(ex);
 
             httpContext.Response.ContentType = "application/json";
-            httpContext.Response.StatusCode = 500;
+
+            int statusCode = 500;
+
+            httpContext.Response.StatusCode = statusCode;
+
+            var responseBody = new EndpointResponse<Empty>
+            {
+                Data = Empty.Value,
+                ErrorMessages = new string[] { _translator.Translate("anErrorOccurred") },
+                StatusCode = statusCode
+            };
+
+            var serializedBody = JsonConvert.SerializeObject(
+                responseBody,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                }
+            );
+
+            await httpContext.Response.WriteAsync(serializedBody);
         }
     }
 }
